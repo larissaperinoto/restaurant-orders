9fc29b50de3b1e4c0d9fa8b3307c9235ae93458f
Larissa Perinoto <perinotolarissa@gmail.com>
2023-04-14 10:45:09 +0200

    test: insere testes para a classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,13 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
-# Req 1
 def test_ingredient():
-    pass
+    instance = Ingredient('lactose')
+    instance2 = Ingredient('seafood')
+
+    assert instance.name == 'lactose'
+    assert hash(instance) == hash('lactose')
+    assert repr(instance) == "Ingredient('lactose')"
+    assert instance == instance
+    assert instance != instance2
+    assert instance.restrictions == set()
