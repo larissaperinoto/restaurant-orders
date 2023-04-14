af07c6a433a395e2ca2d54793d624001e366ace2
Larissa Perinoto <perinotolarissa@gmail.com>
2023-04-14 11:26:27 +0200

    tests: insere testes para a classe Dish

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,28 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import pytest
 
 
-# Req 2
 def test_dish():
-    pass
+    instance = Dish('Abacate', 3.4)
+    another_instance = Dish('Alho', 1.90)
+
+    assert instance.name == 'Abacate'
+
+    with pytest.raises(TypeError, match="Dish price must be float."):
+        Dish('a', '2332')
+
+    error = "Dish price must be greater then zero."
+    with pytest.raises(ValueError, match=error):
+        Dish('a', -3)
+
+    assert repr(instance) == "Dish('Abacate', R$3.40)"
+    assert instance == instance
+    assert instance != another_instance
+    assert hash(instance) == hash(repr(instance))
+    assert hash(instance) != hash(repr(another_instance))
+
+    instance.add_ingredient_dependency(Ingredient('Água'), 200)
+    assert instance.recipe == {Ingredient('Água'): 200}
+    assert instance.get_ingredients() == set(instance.recipe.keys())
+    assert instance.get_restrictions() == set()
